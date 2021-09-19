8579846d82033174b24bd0759a647f145860b727
Charvi Bansal <charvibansal02@gmail.com>
2021-09-20 03:10:48 +0530

    add POST and PUT

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const database = require("./database/index");
 //Initialising express
 const athena = express();
 
-//configurations 
+//configurations
 athena.use(express.json());
-
 
 /* 
 Route          /
@@ -18,10 +17,9 @@ Access         PUBLIC
 Parameters     NONE
 Method         GET
 */
-athena.get("/", (req,res) => {
-    return res.json({books: database.books});
-})
-
+athena.get("/", (req, res) => {
+  return res.json({ books: database.books });
+});
 
 /* 
 Route          /is 
@@ -30,19 +28,19 @@ Access         PUBLIC
 Parameters     isbn
 Method         GET
 */
-athena.get("/is/:isbn", (req,res) => {
-    const getSpecificBook = database.books.filter(
-        (book) => book.ISBN === req.params.isbn
-    );
+athena.get("/is/:isbn", (req, res) => {
+  const getSpecificBook = database.books.filter(
+    (book) => book.ISBN === req.params.isbn
+  );
 
-    if(getSpecificBook.length === 0){
-        return res.json({error: `No specific book found for the ISBN ${req.params.isbn}`})
-    }
+  if (getSpecificBook.length === 0) {
+    return res.json({
+      error: `No specific book found for the ISBN ${req.params.isbn}`,
+    });
+  }
 
-    return res.json({book: getSpecificBook});
-
+  return res.json({ book: getSpecificBook });
 });
-
 
 /* 
 Route          /c
@@ -51,18 +49,19 @@ Access         PUBLIC
 Parameters     category
 Method         GET
 */
-athena.get("/c/:category", (req,res) => {
-    const getSpecificBooks = database.books.filter(
-        (book) => book.category.includes(req.params.category)
-    );
+athena.get("/c/:category", (req, res) => {
+  const getSpecificBooks = database.books.filter((book) =>
+    book.category.includes(req.params.category)
+  );
 
-    if(getSpecificBooks.length === 0){
-        return res.json({error: `No books found for the category ${req.params.category}`})
-    }
+  if (getSpecificBooks.length === 0) {
+    return res.json({
+      error: `No books found for the category ${req.params.category}`,
+    });
+  }
 
-    return res.json({book: getSpecificBooks});
-})
-
+  return res.json({ book: getSpecificBooks });
+});
 
 /* 
 Route          /a
@@ -71,18 +70,19 @@ Access         PUBLIC
 Parameters     authors
 Method         GET
 */
-athena.get("/a/:authors", (req,res) => {
-    const getSpecificBooks = database.books.filter(
-        (book) => book.authors.includes(parseInt(req.params.authors))
-    );
+athena.get("/a/:authors", (req, res) => {
+  const getSpecificBooks = database.books.filter((book) =>
+    book.authors.includes(parseInt(req.params.authors))
+  );
 
-    if(getSpecificBooks.length === 0){
-        return res.json({error: `No books found for author ${req.params.authors}`})
-    }
+  if (getSpecificBooks.length === 0) {
+    return res.json({
+      error: `No books found for author ${req.params.authors}`,
+    });
+  }
 
-    return res.json({book: getSpecificBooks});
-})
-
+  return res.json({ book: getSpecificBooks });
+});
 
 /* 
 Route          /author
@@ -91,10 +91,9 @@ Access         PUBLIC
 Parameters     NONE
 Method         GET
 */
-athena.get("/author", (req,res) => {
-    return res.json({authors: database.authors});
-})
-
+athena.get("/author", (req, res) => {
+  return res.json({ authors: database.authors });
+});
 
 /* 
 Route          /authid 
@@ -103,19 +102,19 @@ Access         PUBLIC
 Parameters     id
 Method         GET
 */
-athena.get("/authid/:id", (req,res) => {
-    const getSpecificAuthor = database.authors.filter(
-        (author) => author.id == req.params.id
-    );
+athena.get("/authid/:id", (req, res) => {
+  const getSpecificAuthor = database.authors.filter(
+    (author) => author.id == req.params.id
+  );
 
-    if(getSpecificAuthor.length === 0){
-        return res.json({error: `No specific author found for the id ${req.params.id}`})
-    }
+  if (getSpecificAuthor.length === 0) {
+    return res.json({
+      error: `No specific author found for the id ${req.params.id}`,
+    });
+  }
 
-    return res.json({author: getSpecificAuthor});
-
+  return res.json({ author: getSpecificAuthor });
 });
-
 
 /* 
 Route          /author
@@ -124,17 +123,17 @@ Access         PUBLIC
 Parameters     isbn
 Method         GET
 */
-athena.get("/author/:isbn",(req,res) => {
-    const getSpecificAuthors = database.authors.filter((author) => 
-        author.books.includes(req.params.isbn)
-    );
+athena.get("/author/:isbn", (req, res) => {
+  const getSpecificAuthors = database.authors.filter((author) =>
+    author.books.includes(req.params.isbn)
+  );
 
-    if(getSpecificAuthors.length === 0){
-        return res.json({error: `No authors found for book ${req.params.isbn}`})
-    }
+  if (getSpecificAuthors.length === 0) {
+    return res.json({ error: `No authors found for book ${req.params.isbn}` });
+  }
 
-    return res.json({authors: getSpecificAuthors})
-})
+  return res.json({ authors: getSpecificAuthors });
+});
 
 /* 
 Route          /publications
@@ -143,10 +142,9 @@ Access         PUBLIC
 Parameters     NONE
 Method         GET
 */
-athena.get("/publications", (req,res) => {
-    return res.json({publications: database.publications});
-})
-
+athena.get("/publications", (req, res) => {
+  return res.json({ publications: database.publications });
+});
 
 /* 
 Route          /pubid 
@@ -155,19 +153,19 @@ Access         PUBLIC
 Parameters     id
 Method         GET
 */
-athena.get("/pubid/:id", (req,res) => {
-    const getSpecificPublication = database.publications.filter(
-        (publication) => publication.id == req.params.id
-    );
+athena.get("/pubid/:id", (req, res) => {
+  const getSpecificPublication = database.publications.filter(
+    (publication) => publication.id == req.params.id
+  );
 
-    if(getSpecificPublication.length === 0){
-        return res.json({error: `No specific publication found for the id ${req.params.id}`})
-    }
+  if (getSpecificPublication.length === 0) {
+    return res.json({
+      error: `No specific publication found for the id ${req.params.id}`,
+    });
+  }
 
-    return res.json({author: getSpecificPublication});
-
+  return res.json({ author: getSpecificPublication });
 });
-
 
 /* 
 Route          /publications
@@ -176,17 +174,104 @@ Access         PUBLIC
 Parameters     isbn
 Method         GET
 */
-athena.get("/publications/:isbn",(req,res) => {
-    const getSpecificPublications = database.authors.filter((publication) => 
+athena.get("/publications/:isbn", (req, res) => {
+  const getSpecificPublications = database.authors.filter((publication) =>
     publication.books.includes(req.params.isbn)
-    );
+  );
 
-    if(getSpecificPublications.length === 0){
-        return res.json({error: `No publications found for book ${req.params.isbn}`})
+  if (getSpecificPublications.length === 0) {
+    return res.json({
+      error: `No publications found for book ${req.params.isbn}`,
+    });
+  }
+
+  return res.json({ publications: getSpecificPublications });
+});
+
+/* 
+Route          /book/new
+Description    add new book
+Access         PUBLIC
+Parameters     NONE
+Method         POST
+*/
+athena.post("/book/new", (req, res) => {
+  const { newBook } = req.body;
+  database.books.push(newBook);
+  return res.json({ books: database.books, message: "book was added!" });
+});
+
+/* 
+Route          /author/new
+Description    add new author
+Access         PUBLIC
+Parameters     NONE
+Method         POST
+*/
+athena.post("/author/new", (req, res) => {
+  const { newAuthor } = req.body;
+  database.authors.push(newAuthor);
+  return res.json({ authors: database.authors, message: "author was added!" });
+});
+
+/* 
+Route          /publication/new
+Description    add new publication
+Access         PUBLIC
+Parameters     NONE
+Method         POST
+*/
+athena.post("/publication/new", (req, res) => {
+  const { newPublication } = req.body;
+  database.publications.push(newPublication);
+  return res.json({
+    publications: database.publications,
+    message: "publication was added!",
+  });
+});
+
+/* 
+Route          /book/update
+Description    update book title
+Access         PUBLIC
+Parameters     isbn
+Method         PUT
+*/
+athena.put("/book/update/:isbn", (req, res) => {
+  database.books.forEach((book) => {
+    if (book.ISBN === req.params.isbn) {
+      book.title = req.body.bookTitle;
+      return;
     }
+  });
+  return res.json({ book: database.books, message: "book title was updated!" });
+});
 
-    return res.json({publications: getSpecificPublications})
-})
+/* 
+Route          /book/author/update
+Description    update/add new author
+Access         PUBLIC
+Parameters     isbn
+Method         PUT
+*/
+athena.put("/book/author/update/:isbn", (req, res) => {
+  //update book database
+  database.books.forEach((book) => {
+    if (book.ISBN === req.params.isbn)
+      return book.authors.push(req.body.newAuthor);
+  });
 
+  //update author database
+  database.authors.forEach((author) => {
+    if (author.id === req.body.newAuthor)
+      return author.books.push(req.params.isbn);
+  });
+
+  return res.json({
+    books: database.books,
+    authors: database.authors,
+    message: "New author was added!",
+  });
+});
 
 athena.listen(3000, () => console.log("Server running!"));
--- requirements.js
+++ requirements.js
@@ -14,21 +14,71 @@
 
 //Requirements
 
+
+// -------------------------------------------------------------------------------------------
+
 //For books
+
+//GET
 //we need an API
 //to get all books [done]
 //to get specific book [done]
 //to get a list of books based on category [done]
 //to get list of books based on author [done by myself]
 
+//POST
+//New book [done]
+
+//PUT
+// Update book details(title) [done]
+// update/add new author [done]
+
+//DELETE
+// delete a book
+// delete an author from the book
+
+
+// -------------------------------------------------------------------------------------------
+
+
 //For authors
+
+//GET
 //we need an API 
 //to get all authors [done]
 //to get a specific author based on id [done]
 //to get a list of authors based on a book [done]
 
+//POST
+// add new author [done]
+
+//PUT
+// update author name using id [task]
+
+
+//DELETE
+// delete an author from the database itself    
+
+
+// -------------------------------------------------------------------------------------------
+
+
 //For publications
+
+//GET
 //we need an API
 //to get all publications [done]
 //to get a specific publications based on id [done]
 //to get a list of publications based on a book's isbn [done]
+
+//POST
+// Add new publication [done]
+
+//PUT
+// update publication name using id [task]
+// update/add new book to a publication
+
+//DELETE
+// delete a book from publication
+// delete a publication
+
